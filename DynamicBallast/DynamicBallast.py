0036f34373553169e022effeed7a4b5e1bee58c2
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2021-01-09 17:23:02 +0200

    add function for calculating penalties

--- DynamicBallast/DynamicBallast.py
+++ DynamicBallast/DynamicBallast.py
@@ -3,11 +3,14 @@ import ac
 import acsys
 import time
 
-
 appName = "Dynamic Ballast"
 width, height = 400, 400 # width and height of the app's window
 
-UPDATE_INTERVAL = 10.0 # Seconds
+UPDATE_INTERVAL = 5.0 # Seconds
+MAX_PENALTY_PROGRESS_DELTA = 0.5
+PENALTY_BALLAST_MAX = 500
+PENALTY_RESTRICTOR_MAX = 100
+
 
 def acMain(ac_version):
     global appWindow, last_updated
@@ -21,21 +24,38 @@ def acMain(ac_version):
     return appName
 
 
-def calculate_progress_metrics():
+def calculate_progresses():
     ncars = ac.getCarsCount()
-    d = {}
+    d = []
     for i in range(ncars):
         lap = ac.getCarState(i, acsys.CS.LapCount)
         spline = ac.getCarState(i, acsys.CS.NormalizedSplinePosition)
         prg = lap + spline
-        d[i] = prg
+        d.append(prg)
     return d
+
+
+def calculate_penalties(progresses):
+    prog_min = min(progresses)
+    prog_max = max(progresses)
+
+    advantages = [p - prog_min for p in progresses]
+    scale = max(0.001, prog_max - prog_min)
+    dampen = min(1.0, (prog_max - prog_min) / MAX_PENALTY_PROGRESS_DELTA)
+
+    penalties = [dampen * (a/scale) for a in advantages]
+    return penalties
+
 
 def acUpdate(deltaT):
     global last_updated
 
     if (time.clock() - last_updated) > UPDATE_INTERVAL:
-        metrics = calculate_progress_metrics()
-        ac.console("Player progress: {}".format(metrics))
+        progresses = calculate_progresses()
+        penalties = calculate_penalties(progresses)
+
+        ac.concole("")
+        ac.console("Player progresses: {}".format(progresses))
+        ac.console("Player penalties: {}".format(penalties))
 
         last_updated = time.clock()
