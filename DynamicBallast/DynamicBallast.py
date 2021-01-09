9488c20a45e5cf44f423d2a7b6a0bc223630b922
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2021-01-09 15:46:54 +0200

    calculate progress metrics

--- DynamicBallast/DynamicBallast.py
+++ DynamicBallast/DynamicBallast.py
@@ -3,10 +3,11 @@ import ac
 import acsys
 import time
 
+
 appName = "Dynamic Ballast"
 width, height = 400, 400 # width and height of the app's window
 
-UPDATE_INTERVAL = 5.0 # Seconds
+UPDATE_INTERVAL = 10.0 # Seconds
 
 def acMain(ac_version):
     global appWindow, last_updated
@@ -15,19 +16,26 @@ def acMain(ac_version):
     ac.setTitle(appWindow, appName)
     ac.setSize(appWindow, width, height)
 
-    ac.console("Dynamic Ballast running")
-
     last_updated = time.clock()
-
+    
     return appName
 
+
+def calculate_progress_metrics():
+    ncars = ac.getCarsCount()
+    d = {}
+    for i in range(ncars):
+        lap = ac.getCarState(i, acsys.CS.LapCount)
+        spline = ac.getCarState(i, acsys.CS.NormalizedSplinePosition)
+        prg = lap + spline
+        d[i] = prg
+    return d
 
 def acUpdate(deltaT):
     global last_updated
 
     if (time.clock() - last_updated) > UPDATE_INTERVAL:
-        msg = "last updated {} seconds ago".format(last_updated)
-        ac.sendChatMessage(msg)
-        ac.console(msg)
+        metrics = calculate_progress_metrics()
+        ac.console("Player progress: {}".format(metrics))
 
         last_updated = time.clock()
