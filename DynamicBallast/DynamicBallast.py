dc56a24f948496a9d3e54cd5a20cb1849acb4e01
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2021-01-10 00:01:01 +0200

    separate posting and calculation intervals, works now

--- DynamicBallast/DynamicBallast.py
+++ DynamicBallast/DynamicBallast.py
@@ -6,27 +6,32 @@ import time
 appName = "Dynamic Ballast"
 width, height = 400, 400 # width and height of the app's window
 
-UPDATE_INTERVAL = 1.0 # Seconds
-MAX_PENALTY_PROGRESS_DELTA = 0.5
-PENALTY_BALLAST_MAX = 500
+CALCULATION_INTERVAL = 10.0 # Seconds
+POSTING_INTERVAL = 2.0 # Seconds
+
+MAX_PENALTY_PROGRESS_DELTA = 0.05
+PENALTY_BALLAST_MAX = 2000
 PENALTY_RESTRICTOR_MAX = 100
 
 NAME_TO_GRID_ID = {
     'Jesse': 0,
     'nikoj0': 1,
-    'Jonas': 2,
-    'bupu7': 3
+    'squeakymousetoy': 2,
+    'bupu7': 
 }
 
 def acMain(ac_version):
-    global appWindow, last_updated
+    global appWindow, last_calculated, last_posted
 
     appWindow = ac.newApp(appName)
     ac.setTitle(appWindow, appName)
     ac.setSize(appWindow, width, height)
 
-    last_updated = time.clock()
-    
+    last_calculated = time.clock()
+    last_posted = time.clock()
+
+    ac.sendChatMessage("/admin xxxxxxxx")
+
     return appName
 
 
@@ -57,16 +62,17 @@ def calculate_penalties(progresses):
 
 
 def acUpdate(deltaT):
-    global last_updated
+    global last_calculated, last_posted, msg_queue
 
-    if (time.clock() - last_updated) > UPDATE_INTERVAL:
+    if (time.clock() - last_calculated) > CALCULATION_INTERVAL:
+        last_calculated = time.clock()
         progresses, names = get_progresses_and_names()
         penalties = calculate_penalties(progresses)
 
-        ac.console("")
         ac.console("Player progresses: {}".format(progresses))
         ac.console("Player penalties: {}".format(penalties))
 
+        msg_queue = []
         for i, p in enumerate(penalties):
             bst = int(p*PENALTY_BALLAST_MAX)
             rst = int(p*PENALTY_RESTRICTOR_MAX)
@@ -74,12 +80,11 @@ def acUpdate(deltaT):
             car_name = names[i]
             grid_id = NAME_TO_GRID_ID.get(car_name, "NA")
 
-            msg_bst = "/ballast {} {}".format(grid_id, bst)
-            msg_rst = "/restrictor {} {}".format(grid_id, rst)
+            msg_queue.append("/ballast {} {}".format(grid_id, bst))
+            msg_queue.append("/restrictor {} {}".format(grid_id, rst))
 
-            ac.console(msg_bst)
-            ac.console(msg_rst)
-            ac.sendChatMessage(msg_bst)
-            ac.sendChatMessage(msg_rst)
-            
-        last_updated = time.clock()
+    if (time.clock() - last_posted) > POSTING_INTERVAL:
+        last_posted = time.clock()
+        msg = msg_queue.pop()
+        ac.console(msg)
+        ac.sendChatMessage(msg)
