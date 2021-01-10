6626424336b5ecab6c0f1c26a8e52f7ed82ebbcb
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2021-01-10 12:09:50 +0200

    add ui components

--- DynamicBallast/DynamicBallast.py
+++ DynamicBallast/DynamicBallast.py
@@ -17,7 +17,7 @@ NAME_TO_GRID_ID = {
     'Jesse': 0,
     'nikoj0': 1,
     'squeakymousetoy': 2,
-    'bupu7': 
+    'bupu7': 3
 }
 
 def acMain(ac_version):
@@ -30,9 +30,73 @@ def acMain(ac_version):
     last_calculated = time.clock()
     last_posted = time.clock()
 
+    create_ui_components()
+
     ac.sendChatMessage("/admin xxxxxxxx")
 
     return appName
+
+
+def create_ui_components():
+    global appWindow
+    global spinner_ballast, spinner_restrictor, spinner_smoothing, spinner_calculation_interval, spinner_posting_interval
+    global label_names, label_track_progress, label_penalties, label_last_message
+
+    x_margin = 10
+    y_margin = 80
+    spacing = 16
+
+    # Spinners
+    spinner_ballast = ac.addSpinner(appWindow, "Max. ballast penalty (kg)")
+    ac.setRange(spinner_ballast, 0, 2000)
+    ac.setStep(spinner_ballast, 100)
+    ac.setValue(spinner_ballast, 1000)
+
+    spinner_restrictor = ac.addSpinner(appWindow, "Max. restrictor penalty (pct.)")
+    ac.setRange(spinner_restrictor, 0, 100)
+    ac.setStep(spinner_restrictor, 5)
+    ac.setValue(spinner_restrictor, 90)
+
+    spinner_smoothing = ac.addSpinner(appWindow, "Smoothing (pct. of track)")
+    ac.setRange(spinner_smoothing, 0, 100)
+    ac.setStep(spinner_smoothing, 5)
+    ac.setValue(spinner_smoothing, 20)
+    
+    spinner_calculation_interval = ac.addSpinner(appWindow, "Calculation interval (ms)")
+    ac.setRange(spinner_calculation_interval, 500, 20000)
+    ac.setStep(spinner_calculation_interval, 500)
+    ac.setValue(spinner_calculation_interval, 5000)
+
+    spinner_posting_interval = ac.addSpinner(appWindow, "Posting interval (ms)")
+    ac.setRange(spinner_posting_interval, 200, 2000)
+    ac.setStep(spinner_posting_interval, 100)
+    ac.setValue(spinner_posting_interval, 1000)
+
+
+    # Text output
+    label_names = ac.addLabel(appWindow, "Driver names:")
+    ac.setFontSize(label_names, 10)
+
+    label_track_progress = ac.addLabel(appWindow, "Track progresses:")
+    ac.setFontSize(label_track_progress, 10)
+
+    label_penalties = ac.addLabel(appWindow, "Penalty percentages:")
+    ac.setFontSize(label_penalties, 10)
+
+    label_last_message = ac.addLabel(appWindow, "Last message posted:")
+    ac.setFontSize(label_last_message, 10)
+
+
+    # Component positions
+    ac.setPosition(spinner_ballast, x_margin, y_margin + (spacing * 0))
+    ac.setPosition(spinner_restrictor, x_margin, y_margin + (spacing * 5))
+    ac.setPosition(spinner_smoothing, x_margin, y_margin + (spacing * 10))
+    ac.setPosition(spinner_calculation_interval, x_margin, y_margin + (spacing * 15))
+    ac.setPosition(spinner_posting_interval, x_margin, y_margin + (spacing * 20))
+    ac.setPosition(label_names, x_margin, y_margin + (spacing * 25))
+    ac.setPosition(label_track_progress, x_margin, y_margin + (spacing * 26))
+    ac.setPosition(label_penalties, x_margin, y_margin + (spacing * 27))
+    ac.setPosition(label_last_message, x_margin, y_margin + (spacing * 29))
 
 
 def get_progresses_and_names():
